9e9aeff0b8b331261610048d924698fcdebda654
Mann Jadwani <mannjadwani2004@gmail.com>
2023-09-20 21:41:58 +0530

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  `mongodb+srv://mannjadwani:${encodeURIComponent('MillionBlue@273')}@cluster0.chgwoqj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
